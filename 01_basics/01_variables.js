41c9813188c5348537e95080649d243a60a70304
Rafiul Islam <irafiul210@gmail.com>
2026-09-11 13:21:28 +0000

    end_of_video_04

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144558
+let accountEmail = "irafiul@gmail.com"
+var accountPassword = "12358"
+accountCity = "Naogaon"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "the@js.com"
+accountPassword = "1234567"
+accountCity = "Rajshahi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
