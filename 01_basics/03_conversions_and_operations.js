27c9ea92b343ad18f5e91de8ac2d18185a2d2586
Rafiul Islam <irafiul210@gmail.com>
2026-09-11 14:05:44 +0000

    coversions of data types

---
+++ 01_basics/03_conversions_and_operations.js
@@ -0,0 +1,36 @@
+let score = "30abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => Nan (the type of NaN is 'number')
+
+true => 1; false => 0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false;
+"" => false
+"anything_including_space" => true
+*/ 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
