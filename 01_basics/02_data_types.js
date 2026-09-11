50537ff7c951e4d6009def79e03dbc17566b7823
Rafiul Islam <irafiul210@gmail.com>
2026-09-11 13:49:55 +0000

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) we are using Node.js, not browser.
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true /false
+// undefined
+// null => standalone value
+// symbol => unique
+
+
+
+// object
+console.log(typeof null);
