99ce86f6cd6afae01a42075c217006a09d3af0f7
Rafiul Islam <irafiul210@gmail.com>
2026-09-12 05:48:53 +0000

    comparison note

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,5 @@
+/*
+Comparison operators and Equality operators behaves and convert things in a different way. The method is not the same for these two.
+
+- Comparison between two different data types is completely avoidable.
+*/ 
