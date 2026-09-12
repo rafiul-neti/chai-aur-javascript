4c3d994a3fede005f13bd76b5d0cccde02949e9a
Rafiul Islam <irafiul210@gmail.com>
2026-09-12 06:31:16 +0000

    data type summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+/*
+- there are two categories of data in JavaScript based on how they store on the memory and how to access them. Primitive and Non-Primitive.
+*/ 
+
+/*
+Primitive:
+String, Number, Boolean, Null, Undefined, Symbol (is used to make a value unique), BigInt
+*/
+
+/*
+Reference Type or Non-Primitive:
+Array, Object, Function
+*/ 
+
+// JavaScript is a dynamically typed language. This means that type checking is performed at runtime (while the program is running) rather than at compile time.
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log({id, anotherId});
+
+console.log(id === anotherId);
+console.log(id == anotherId);
+
