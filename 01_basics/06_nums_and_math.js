58a5be906e96980d705f952cb359f5fc5b85cec5
Rafiul Islam <irafiul210@gmail.com>
2026-09-12 08:41:23 +0000

    number and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(balance.toString());
+
+// toPrecision()
+const otherNumber = 25.3332
+// console.log(otherNumber.toPrecision(2));
+
+
+
+// ++++++++++++++++++++++++++ math ++++++++++++++++++++++++
+// abs(): only negative values convert to positive
+console.log(Math.abs(-4));
+
+// round()
+console.log(Math.round(4.5)); //=> 5
+
+// ceil()
+console.log(Math.ceil(4.1)); //=> 5
+
+// floor()
+console.log(Math.floor(4.9)); //=> 4
+
+// random()
+console.log(Math.random()); //=> always something between 0 and 1
